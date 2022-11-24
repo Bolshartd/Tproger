b03584114d61edf2544c08d403006d7ed9a031fa
Bolshartd <bolshartd@gmail.com>
2022-11-24 13:14:17 +0300

    v2 Matching months to season

--- season_from_month.py
+++ season_from_month.py
@@ -1,12 +1,12 @@
 import calendar
 def season(month):
-    if (month == 'December') or (month == 'January') or (month == 'February'):
+    if month in ('December', 'January', 'February'):
         return ('Winter')
-    elif (month == 'March') or (month == 'April') or (month == 'May'):
+    elif month in ('March', 'April', 'May'):
         return ('Spring')
-    elif (month == 'June') or (month == 'July') or (month == 'August'):
+    elif month in ('June', 'July', 'August'):
         return ('Summer')
-    elif (month == 'September') or (month == 'October') or (month == 'November'):
+    elif month in ('September', 'October', 'November'):
         return ('Fall')
 
 month = calendar.month_name[int(input("""Write a number from 1 to 12, where 1 -> January and 12 -> December\nNumber:"""))]
