a284c505d3cc8a44b2bddb175348dbab483f2adb
Bolshartd <bolshartd@gmail.com>
2022-12-05 15:03:07 +0300

    v3 The bank deposit

--- bank.py
+++ bank.py
@@ -1,7 +1,7 @@
-#interest rate a year -> 10%
+#v1
 def bank(cash, year):
     year_count = year/year 
-    rate = int(cash*0.1) 
+    rate = int(cash*0.1) #interest rate a year -> 10%
     profit = cash+rate 
     while year_count<=year:
         if year_count == year:
@@ -11,3 +11,24 @@ def bank(cash, year):
         profit += b 
     
 full = bank(2000, 2)
+
+#v2
+class Start_stack:
+    def __init__(self, cash, year):
+        self.cash = cash
+        self.year = year
+    
+class Profit:
+    def __init__(self, cash, year):
+        self.perc = int(full.cash*0.1) #interest rate a year -> 10%
+        self.cash = full.cash+self.perc 
+        self.year_count = full.year/full.year 
+        while self.year_count <= full.year:
+            if self.year_count == full.year:
+                print(f'Your profit for {year} years will be: {self.cash}')
+            self.year_count += 1 
+            self.perc = int(self.cash*0.1) 
+            self.cash += self.perc 
+            
+full = Start_stack(1000, 5)
+percent = Profit(full.cash, full.year)
