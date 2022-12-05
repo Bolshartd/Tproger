571c81e56d2bb421ce7e1f44c970f1e08a0c078d
Bolshartd <bolshartd@gmail.com>
2022-12-05 12:58:04 +0300

    v3 Calendar module

--- calendar_module.py
+++ calendar_module.py
@@ -2,10 +2,10 @@ import calendar
 
 def is_year_leap(year):
     if calendar.isleap(year) is True:
-        return(f'\nThis year is leap year: {calendar.isleap(year)}')
+        return(f'\nThis year is a leap year: {calendar.isleap(year)}')
         
     else:
-        return(f'\nThis year is not leap year: {calendar.isleap(year)}')
+        return(f'\nThis year is not a leap year: {calendar.isleap(year)}')
         
-year = int(input('Write a year to check is it leap: '))
+year = int(input('Write a year to check is it a leap: '))
 print(is_year_leap(year))
